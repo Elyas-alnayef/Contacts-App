534f1cbbc08a3ae2b81db854006e46b77d62bfb6
Elyas-alnayef <elyasalnayef@gmail.com>
2024-07-04 16:12:18 +0300

    ...

--- lib/features/contacts/presentation/pages/contact_profile_page.dart
+++ lib/features/contacts/presentation/pages/contact_profile_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/widgets/appbar.dart';
+import '../../../../core/widgets/drawer.dart';
+
 class ContactProfilePage extends StatelessWidget {
   const ContactProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      drawer: const DrawerWidget(),
+      appBar: const AppBarWidget(),
+    );
   }
 }
